f030bc7fdc966db732ab5f68cce91d3c3f0fbc07
Markus Möller <markus.moeller@terrasys.info>
2020-06-04 12:48:16 +0200

    Update fZonaSt.R

--- functions/fZonaSt.R
+++ functions/fZonaSt.R
@@ -1,36 +1,54 @@
-#######################################################################################################
-#######################################################################################################
-#######################################################################################################
-print("Function for zonal statistics")
-#######################################################################################################
-#######################################################################################################
-#######################################################################################################
-fZonaSt <- function(TA.DIR,
-                    TA,
-                    POLYGON.DIR,
-                    POLYGON.SHP,
-                    OUT.DIR){
-  setwd(file.path(POLYGON.DIR))
-  #Import reference units)
-  o <- st_read(paste(POLYGON.SHP,".shp",sep=""))
-  o$ID <- 1:nrow(o)
-  write_sf(o, paste(POLYGON.SHP,"_",TA,".shp",sep=""), delete_layer = TRUE)
-  #names of original attributes shape file
-  st_geometry(o) <- NULL
-  names.s <- names(o)
-  #-----------------------------------------------------------------------------------------------------
-  print("Zonal statistic")
-  #-----------------------------------------------------------------------------------------------------
-  setwd(TA.DIR)
-  l.g <- mixedsort(list.files(pattern=paste("^(",TA,").*\\.sgrd$",sep="")),decreasing=FALSE)
-  #loop
-  pb <- txtProgressBar(min=0, max=length(l.g), style=3)
+print("Zonal statistics of raster files")
+#########################################################################################################
+##Libraries and functions
+#########################################################################################################
+loadandinstall <- function(mypkg) {
+  for(i in seq(along=mypkg)){
+    if (!is.element(mypkg[i],installed.packages()[,1])){install.packages(mypkg[i])}
+    library(mypkg[i], character.only=TRUE)
+  }
+}
+packages <- sort(c("raster",
+                   "rgdal",
+                   "radiant.data", 
+                   "rgeos",
+                   "RSAGA",
+                   "sf",
+                   "sp",
+                   "stats",
+                   "stringr",
+                   "tidyr",
+                   "utils"))
+loadandinstall(packages)
+
+
+##Setting RSAGA environment
+myenv <- rsaga.env(workspace="c:/Users/scien/_temp/", 
+                   path="c:/_saga_222_x64",
+                   modules = "c:/_saga_222_x64/modules")
+print("RSAGA modules")
+print(rsaga.get.libraries(path= myenv$modules))
+rsaga.get.lib.modules("ta_lighting", env =  myenv, interactive = FALSE)
+rsaga.get.usage("ta_lighting",0,env =  myenv)
+
+#########################################################################################################
+##Function
+#########################################################################################################
+fZonaSt <- function(RASTER.DIR,
+                    RASTER.PF,
+                    RU.DIR,
+                    RU.SHP,
+                    PM.PF){
+setwd(RASTER.DIR)
+l.g <- mixedsort(list.files(pattern=paste("^(",PM.PF,").*\\.sgrd$",sep="")),decreasing=FALSE)
+#loop
+pb <- txtProgressBar(min=0, max=length(l.g), style=3)
   for (i in 1:length(l.g)){
     rsaga.geoprocessor(
       lib="shapes_grid",
       module=2,
-      param=list(GRIDS=file.path(TA.DIR,l.g[i]),
-                 POLYGONS=paste(POLYGON.DIR,POLYGON.SHP,"_",TA,".shp",sep=""),
+      param=list(GRIDS=file.path(RASTER.DIR,l.g[i]),
+                 POLYGONS=paste(RU.DIR,RU.SHP,".shp",sep=""),
                  COUNT=0,
                  MIN=0,
                  MAX=0,
@@ -43,18 +61,4 @@ fZonaSt <- function(TA.DIR,
       env=myenv)
     setTxtProgressBar(pb, i)
   }
- #-----------------------------------------------------------------------------------------------------
- print("Import and rename attributed shape file attributes")
- #-----------------------------------------------------------------------------------------------------
- o <- st_read(paste(OUT.DIR,POLYGON.SHP,"_",TA,".shp",sep=""))
- colnames(o) <- c(names.s,paste(str_sub(l.g, 1,-6)),paste("geometry"))
- o$ID <- 1:nrow(o)
- write_sf(o, paste(OUT.DIR,POLYGON.SHP,"_",TA,".shp",sep=""), delete_layer = TRUE)
- 
- #------------------------------------------------------------------------------------------------------
- #Correlation matrix
- #------------------------------------------------------------------------------------------------------
- st_geometry(o) <- NULL
- write.csv2(cor(as.matrix(o[grepl(paste(TA,sep=""), names(o))])),
-            file=paste(OUT.DIR,POLYGON.SHP,"_",TA,"_CorMatr.csv",sep=""))
- }
+}
