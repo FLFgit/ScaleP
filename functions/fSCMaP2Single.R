932d5c5728efa1326269a15038c88ff0a5681671
Markus Möller <markus.moeller@terrasys.info>
2020-08-08 18:29:27 +0200

    Update and rename fSCMaP2SAGA.R to fSCMaP2Single.R

--- functions/fSCMaP2Single.R
+++ functions/fSCMaP2Single.R
@@ -1,4 +1,4 @@
-print("Converting multi.dimensional SCMaP file to single SAGA GRID files")
+print("Converting multi-dimensional SCMaP file to single raster files")
 #########################################################################################################
 ##Libraries
 #########################################################################################################
@@ -8,43 +8,68 @@ loadandinstall <- function(mypkg) {
     library(mypkg[i], character.only=TRUE)
   }
 }
-packages <- sort(c("raster"))
+packages <- sort(c("raster","BBmisc"))
 loadandinstall(packages)
+#function to normalize raster files
+rescale <- function(x, x.min = NULL, x.max = NULL, new.min = 0, new.max = 1) {
+  if(is.null(x.min)) x.min = min(x)
+  if(is.null(x.max)) x.max = max(x)
+  new.min + (x - x.min) * ((new.max - new.min) / (x.max - x.min))
+}
 #########################################################################################################
 ##Function
 #########################################################################################################
-fSCMaP2SAGA <- function(SCMaP.DIR,
+fSCMaP2Single <- function(SCMaP.DIR,
                        SCMaP.FILE,
                        SCMaP.PF,
                        SCMaP.FRM,
                        OUT.DIR,
-                       EXTENT=TRUE){
+                       EXTENT=TRUE,
+                       RESCALE=TRUE,
+                       OUT.FRM){
 #-----------------------------------------------------------------------------------------------------
 print("Import SCMaP file")
 #-----------------------------------------------------------------------------------------------------
 r <- stack(paste(SCMaP.DIR,SCMaP.FILE,SCMaP.FRM,sep=""))
-
 if(EXTENT==TRUE){
 #-----------------------------------------------------------------------------------------------------
-print("Export extent shape file")
+print("Optional: Export extent shape file")
 #-----------------------------------------------------------------------------------------------------
 e <- extent(r)
 p <- as(e, 'SpatialPolygons')
 crs(p) <- crs(r)
 setwd(file.path(SCMaP.DIR))
-shapefile(p, "Extent2.shp")
+shapefile(p, paste(SCMaP.FILE,".shp",sep=""))
 }
-
 #-----------------------------------------------------------------------------------------------------
 print("Generating single RS files in SAGA format")
 #-----------------------------------------------------------------------------------------------------
 pb <- txtProgressBar(min=0, max=length(r@layers), style=3)
 for(i in 1:length(r@layers)){
+   #Convert values < 0 to NA
    r[[i]][r[[i]] < 0] <- NA
+   #Optional: Rescale pixel values between 0 and 1
+   if(RESCALE==TRUE){
+  r[[i]] <- rescale(r[[i]], x.min = cellStats(r[[i]],stat = "min", na.rm=TRUE), 
+                   x.max = cellStats(r[[i]],stat = "max", na.rm=TRUE), 
+                   new.min = 0, new.max = 1)
+   }
+   #export file
+   if(OUT.FRM==".sgrd"){
    writeRaster(r[[i]],paste(OUT.DIR,SCMaP.PF,i,sep=""),
                 format="SAGA",
                 overwrite=TRUE)
+   }
+   if(OUT.FRM==".tif"){
+     writeRaster(r[[i]],paste(OUT.DIR,SCMaP.PF,i,sep=""),
+                 format="GTiff",
+                 overwrite=TRUE)
+   }
+   if(OUT.FRM==".asc"){
+     writeRaster(r[[i]],paste(OUT.DIR,SCMaP.PF,i,sep=""),
+                 format="ascii",
+                 overwrite=TRUE)
+   }
     setTxtProgressBar(pb, i)
   }
 }
-
